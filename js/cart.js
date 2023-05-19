203f2e8d6a778e3974df7df3baab7410e9cc03cc
saajida mathen <mathensaajida@gmail.com>
2023-05-19 16:08:25 +0200

    functionalities implemented

--- js/cart.js
+++ js/cart.js
@@ -1,4 +1,3 @@
-
 const cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
 let totalAmount = parseFloat(localStorage.getItem('totalAmount')) || 0;
 
@@ -15,40 +14,74 @@ function removeItem(index) {
   saveCartItems();
 }
 
-function decreaseQuantity() {
-  var input = document.getElementById("quantity");
-  var currentValue = parseInt(input.value);
-  if (currentValue > 0) {
-    input.value = currentValue - 1;
+
+
+function decreaseQuantity(index) {
+  const item = cartItems[index];
+  if (item.quantity > 0) {
+    item.quantity -= 1;
+    item.total = item.itemPrice * item.quantity; // Use item.itemPrice instead of item.price
+    updateCart();
+    saveCartItems();
+    updateTotal();
   }
 }
 
-function increaseQuantity() {
-  var input = document.getElementById("quantity");
-  var currentValue = parseInt(input.value);
-  input.value = currentValue + 1;
+function increaseQuantity(index) {
+  const item = cartItems[index];
+  item.quantity += 1;
+  item.total = item.itemPrice * item.quantity; // Use item.itemPrice instead of item.price
+  updateCart();
+  saveCartItems();
+  updateTotal();
 }
+
+function addToCart(itemName, itemPrice, imageUrl) {
+  const quantity = 1;
+  const total = itemPrice * quantity;
+
+  cartItems.push({ itemName, itemPrice, quantity, total, imageUrl });
+  totalAmount += total;
+
+  updateCart();
+  updateCartCount();
+
+}
+
+function updateTotal() {
+  totalAmount = cartItems.reduce((total, item) => total + item.total, 0);
+  const totalAmountElement = document.getElementById('totalAmount');
+  totalAmountElement.textContent = `Total: R ${totalAmount.toFixed(2)}`;
+}
+
+
+function initTotal() {
+  totalAmount = cartItems.reduce((total, item) => total + item.total, 0);
+  updateTotal();
+}
+
+initTotal();
+
+
 
 function updateCart() {
   const cartContentElement = document.getElementById('cartContent');
-  const totalAmountElement = document.getElementById('totalAmount');
-
   cartContentElement.innerHTML = '';
 
   for (let i = 0; i < cartItems.length; i++) {
     const item = cartItems[i];
     const itemElement = document.createElement('div');
-    itemElement.textContent = `${item.itemName} - Quantity: ${item.quantity} - Total: $${item.total.toFixed(2)}`;
+    itemElement.textContent = `${item.itemName} - Quantity: ${item.quantity} - Total: R ${item.total.toFixed(2)}`;
 
     const decreaseButton = document.createElement('button');
     decreaseButton.className = 'btn btn-secondary';
     decreaseButton.textContent = '-';
-    decreaseButton.onclick = () => decreaseQuantity();
+    decreaseButton.onclick = () => decreaseQuantity(i);
 
     const increaseButton = document.createElement('button');
     increaseButton.className = 'btn btn-secondary';
     increaseButton.textContent = '+';
-    increaseButton.onclick = () => increaseQuantity();
+    increaseButton.onclick = () => increaseQuantity(i);
 
     const removeButton = document.createElement('button');
     removeButton.className = 'btn btn-danger';
@@ -61,8 +94,10 @@ function updateCart() {
     cartContentElement.appendChild(itemElement);
   }
 
-  totalAmountElement.textContent = `Total: $${totalAmount.toFixed(2)}`;
+  totalAmount = cartItems.reduce((total, item) => total + item.total, 0);
+  updateTotal();
 }
+
 
 function saveCartItems() {
   localStorage.setItem('cartItems', JSON.stringify(cartItems));
--- js/index.js
+++ js/index.js
@@ -1,81 +1,31 @@
-// let cartItems = [];
-// let totalAmount = 0;
-
-// function addToCart(itemName, itemPrice) {
-//   const quantity = 1;
-//   const total = itemPrice * quantity;
-
-//   cartItems.push({ itemName, quantity, total });
-//   totalAmount += total;
-
-//   updateCart();
-//   updateCartCount();
-// }
-
-// function removeAllItems() {
-//   cartItems = [];
-//   totalAmount = 0;
-
-//   updateCart();
-//   updateCartCount();
-// }
-
-// function updateCart() {
-//   const cartContent = document.getElementById('cartContent');
-//   cartContent.innerHTML = '';
-
-//   cartItems.forEach((item, index) => {
-//     const itemElement = document.createElement('div');
-//     itemElement.classList.add('item');
-//     itemElement.innerHTML = `Item: ${item.itemName}, Quantity: ${item.quantity}, Total: $${item.total.toFixed(2)}`;
-//     cartContent.appendChild(itemElement);
-//   });
-
-//   const totalAmountElement = document.getElementById('totalAmount');
-//   totalAmountElement.textContent = `Total: $${totalAmount.toFixed(2)}`;
-// }
-
-// function updateCartCount() {
-//   const cartCountElement = document.getElementById('cartCount');
-//   cartCountElement.textContent = cartItems.length;
-// }
-
-// function openCartPage() {
-//   // Save cartItems and totalAmount to local storage or send them to the server
-//   // Redirect to the cart.html page
-//   location.href = '/html/cart.html';
-// }
-
 let cartItems = [];
-let totalAmount = 0;
+  let totalAmount = 0;
 
-function addToCart(itemName, itemPrice) {
-  const quantity = 1;
-  const total = itemPrice * quantity;
+  function addToCart(itemName, itemPrice) {
+    const quantity = 1;
+    const total = itemPrice * quantity;
 
-  cartItems.push({ itemName, quantity, total });
-  totalAmount += total;
+    cartItems.push({ itemName, itemPrice, quantity, total });
+    totalAmount += total;
 
-  updateCart();
-  updateCartCount();
-}
+    updateCart();
+    updateCartCount();
+  }
+  function updateCart() {
+    // Update the cart content and total amount display logic
+    // ...
+  }
 
-function updateCart() {
-  // Update the cart content and total amount display logic
-  // ...
-}
+  function updateCartCount() {
+    const cartCountElement = document.getElementById('cartCount');
+    cartCountElement.textContent = cartItems.length.toString();
+  }
 
-function updateCartCount() {
-  const cartCountElement = document.getElementById('cartCount');
-  cartCountElement.textContent = cartItems.length.toString();
-}
+  function openCartPage() {
+    // Save cartItems and totalAmount to local storage or send them to the server
+    localStorage.setItem('cartItems', JSON.stringify(cartItems));
+    localStorage.setItem('totalAmount', totalAmount.toString());
 
-
-function openCartPage() {
-  // Save cartItems and totalAmount to local storage or send them to the server
-  localStorage.setItem('cartItems', JSON.stringify(cartItems));
-  localStorage.setItem('totalAmount', totalAmount.toString());
-
-  // Redirect to the cart.html page
-  location.href = '/html/cart.html';
-}
+    // Redirect to the cart.html page
+    location.href = '/html/cart.html';
+  }
