2b370a7834e13767d94e10ae0bd19ab07e839f7a
saajida mathen <mathensaajida@gmail.com>
2023-05-22 11:16:20 +0200

    styling cart page

--- js/cart.js
+++ js/cart.js
@@ -1,5 +1,5 @@
-const cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
-let totalAmount = parseFloat(localStorage.getItem('totalAmount')) || 0;
+const cartItems = JSON.parse(localStorage.getItem("cartItems")) || [];
+let totalAmount = parseFloat(localStorage.getItem("totalAmount")) || 0;
 
 function removeAllItems() {
   cartItems.length = 0;
@@ -13,8 +13,6 @@ function removeItem(index) {
   updateCart();
   saveCartItems();
 }
-
-
 
 function decreaseQuantity(index) {
   const item = cartItems[index];
@@ -46,14 +44,23 @@ function addToCart(itemName, itemPrice, imageUrl) {
   updateCart();
   updateCartCount();
 
+  const cartItemImageElement = document.createElement('img');
+  cartItemImageElement.src = imageUrl;
+  cartItemImageElement.alt = itemName;
+
+  const cartItemElement = document.createElement('div');
+  cartItemElement.classList.add('cart-item');
+  cartItemElement.appendChild(cartItemImageElement);
+
+  const cartContentElement = document.getElementById('cartContent');
+  cartContentElement.appendChild(cartItemElement);
 }
 
 function updateTotal() {
   totalAmount = cartItems.reduce((total, item) => total + item.total, 0);
-  const totalAmountElement = document.getElementById('totalAmount');
+  const totalAmountElement = document.getElementById("totalAmount");
   totalAmountElement.textContent = `Total: R ${totalAmount.toFixed(2)}`;
 }
-
 
 function initTotal() {
   totalAmount = cartItems.reduce((total, item) => total + item.total, 0);
@@ -63,45 +70,54 @@ function initTotal() {
 initTotal();
 
 
-
 function updateCart() {
-  const cartContentElement = document.getElementById('cartContent');
-  cartContentElement.innerHTML = '';
+  const cartContentElement = document.getElementById("cartContent");
+  cartContentElement.innerHTML = "";
 
   for (let i = 0; i < cartItems.length; i++) {
     const item = cartItems[i];
-    const itemElement = document.createElement('div');
-    itemElement.textContent = `${item.itemName} - Quantity: ${item.quantity} - Total: R ${item.total.toFixed(2)}`;
 
-    const decreaseButton = document.createElement('button');
-    decreaseButton.className = 'btn btn-secondary';
-    decreaseButton.textContent = '-';
+    const cartItemElement = document.createElement('div');
+    cartItemElement.classList.add('cart-item');
+
+    const cartItemImageElement = document.createElement('img');
+    cartItemImageElement.src = item.imageUrl;
+    cartItemImageElement.alt = item.itemName;
+
+    const cartItemDetailsElement = document.createElement('div');
+    cartItemDetailsElement.classList.add('cart-item-details');
+    cartItemDetailsElement.innerHTML = `
+      <span class="item-name">${item.itemName}</span>
+      <span class="item-quantity">Quantity: ${item.quantity}</span>
+      <span class="item-total">Total: R ${item.total.toFixed(2)}</span>
+    `;
+
+    const decreaseButton = document.createElement("button");
+    decreaseButton.className = "btn btn-secondary";
+    decreaseButton.textContent = "-";
     decreaseButton.onclick = () => decreaseQuantity(i);
 
-    const increaseButton = document.createElement('button');
-    increaseButton.className = 'btn btn-secondary';
-    increaseButton.textContent = '+';
+    const increaseButton = document.createElement("button");
+    increaseButton.className = "btn btn-secondary";
+    increaseButton.textContent = "+";
     increaseButton.onclick = () => increaseQuantity(i);
 
-    const removeButton = document.createElement('button');
-    removeButton.className = 'btn btn-danger';
-    removeButton.textContent = 'Remove Item';
+    const removeButton = document.createElement("button");
+    removeButton.className = "btn btn-danger";
+    removeButton.textContent = "Remove Item";
     removeButton.onclick = () => removeItem(i);
 
-    itemElement.appendChild(decreaseButton);
-    itemElement.appendChild(increaseButton);
-    itemElement.appendChild(removeButton);
-    cartContentElement.appendChild(itemElement);
+    cartItemDetailsElement.appendChild(decreaseButton);
+    cartItemDetailsElement.appendChild(increaseButton);
+    cartItemDetailsElement.appendChild(removeButton);
+
+    cartItemElement.appendChild(cartItemImageElement);
+    cartItemElement.appendChild(cartItemDetailsElement);
+
+    cartContentElement.appendChild(cartItemElement);
   }
 
-  totalAmount = cartItems.reduce((total, item) => total + item.total, 0);
   updateTotal();
-}
-
-
-function saveCartItems() {
-  localStorage.setItem('cartItems', JSON.stringify(cartItems));
-  localStorage.setItem('totalAmount', totalAmount.toString());
 }
 
 updateCart();
